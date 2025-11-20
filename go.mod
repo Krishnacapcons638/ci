6deee69d013b6718eb19de5160c2c8c5e4c35317
Krishnacapcons638 <krishna@capcons.com>
2025-11-20 13:51:12 +0530

    ci testing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Krishnacapcons638/ci
+
+go 1.25.1
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+type input struct {
+	num1 int
+	num2 int
+}
+
+func TestAdd(t *testing.T) {
+	testcases := []struct {
+		name  string
+		inp   input
+		expec int
+	}{
+		{
+			name: "success",
+			inp: input{
+				num1: 1,
+				num2: 2,
+			},
+			expec: 3,
+		}, {
+			name: "fail",
+			inp: input{
+				num1: 1,
+				num2: 5,
+			},
+			expec: 4,
+		},
+	}
+
+	for _, test := range testcases {
+		exact := Add(test.inp.num1, test.inp.num2)
+		t.Run(test.name, func(t *testing.T) {
+			if exact != test.expec {
+				t.Error("bekar hain bhaiya main toh tootha gaya")
+			}
+		})
+	}
+}
